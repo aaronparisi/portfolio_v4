84cd6b6b9dee85e4d1b10e161c056dc246677c8f
aaronparisi <parisi.aaron@gmail.com>
2023-07-15 22:48:49 -0700

    greeting animations working

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,18 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface GreetingProps {
   darkMode: boolean;
 }
 
 const Greeting: React.FC<GreetingProps> = ({ darkMode }) => {
+  const [charIdx, setCharIdx] = useState<number>(-1);
+  const [thoughtIdx, setThoughtIdx] = useState<number>(0);
+  const [typing, setTyping] = useState<boolean>(true);
+
+  const thoughts: string[] = [
+    'Deep in thought.',
+    'Sometimes distraught.',
+    'Overwraught.',
+    'Works a lot.',
+  ];
+
+  const deleteChar = () => {
+    if (charIdx <= -1) {
+      setThoughtIdx((prev) => (prev + 1) % thoughts.length);
+      setTyping(true);
+    } else {
+      setCharIdx((prev) => prev - 1);
+    }
+  };
+  const typeChar = () => {
+    if (charIdx >= thoughts[thoughtIdx].length) {
+      setTyping(false);
+    } else {
+      setCharIdx((prev) => prev + 1);
+    }
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      if (typing) {
+        typeChar();
+      } else {
+        deleteChar();
+      }
+    }, Math.floor(Math.random() * 200) + 20);
+  }, [typing, charIdx]);
+
   return (
-    <section>
+    <section className="greeting">
       <div
         className={`
           self-taught
         `}
       >
         Self-taught.
+      </div>
+      <div
+        className={`
+          deep-thought
+        `}
+      >
+        {thoughts[thoughtIdx].slice(0, charIdx + 1)}_
       </div>
     </section>
   );
