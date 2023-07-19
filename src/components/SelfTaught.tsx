ca63025aab313ebe68e06ca4367cb054964f974c
aaronparisi <parisi.aaron@gmail.com>
2023-07-19 16:57:28 -0700

    moved rainbow slide to react-spring

--- src/components/SelfTaught.tsx
+++ src/components/SelfTaught.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { useTransition, animated, config } from 'react-spring';
+import {
+  useTransition,
+  animated,
+  config,
+  useSpringRef,
+  useSpring,
+  useChain,
+} from 'react-spring';
 
 interface SelfTaughtProps {}
 
 const SelfTaught: React.FC<SelfTaughtProps> = () => {
-  const transitions = useTransition(['Self-taught'], {
+  const entranceRef = useSpringRef();
+  const entranceSpring = useTransition(['Self-taught'], {
     from: {
       fontSize: '30rem',
       transform: 'scale(20) rotate(0deg)',
@@ -17,11 +25,23 @@ const SelfTaught: React.FC<SelfTaughtProps> = () => {
     },
     config: { ...config.wobbly, clamp: true },
   });
+
+  const swipeRef = useSpringRef();
+  const swipeSpring = useSpring({
+    from: { backgroundPosition: '0 0' },
+    to: { backgroundPosition: '115% 0' },
+    config: { duration: 2000 },
+    delay: 500,
+    loop: true,
+  });
+
+  useChain([entranceRef, swipeRef]);
+
   // return <div className="self-taught">Self-taught.</div>;
-  return transitions((style, item) => (
-    <animated.h2 style={style} className="self-taught">
-      {item}
-    </animated.h2>
+  return entranceSpring((style, item) => (
+    <animated.div style={style} className="self-taught">
+      {<animated.h2 style={swipeSpring}>{item}</animated.h2>}
+    </animated.div>
   ));
 };
 
